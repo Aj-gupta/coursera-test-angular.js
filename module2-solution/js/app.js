e8b7cacb6688a83a63d8edd788786096558cf34e
Amit Kumar Gupta <Amit183630@gmail.com>
2020-05-21 20:37:49 +0530

    assignment 2

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -1,39 +1,70 @@
 (function(){
-  angular.module("LunchCheck",[])
+  angular.module("ShoppingListCheckOff",[])
 
-  .controller("LunchCheckController",LunchCheckfun);
+  .controller("ToBuyController",toBuyFun)
+  .controller("AlreadyBoughtController",alreadyBoughtFun)
+  .service("ShoppingService",ShoppingListCheckOffService);
 
-  LunchCheckfun.$inject=['$scope'];
+  toBuyFun.$inject = ['ShoppingService'];
+  function toBuyFun(ShoppingService) {
+      var buyItems = this;
+      buyItems.items =ShoppingService.toBuyitems;
+      buyItems.bought = function (index) {
+        ShoppingService.boughtfun(index);
+        buyItems.toby = false;
+        //value = false;
+        ShoppingService.tobgvalue = false;
+        if(ShoppingService.toBuyitems.length == 0)
+        {
+          buyItems.toby = true;
+        }
+      };
 
-  function LunchCheckfun($scope){
-    $scope.name='';
-    $scope.message='';
-    $scope.words=[''];
-    $scope.customStyle = { };
-    $scope.custom1Style = { };
-    $scope.check=function()
+
+
+  }
+
+  alreadyBoughtFun.$inject = ['ShoppingService'];
+  function alreadyBoughtFun(ShoppingService){
+    var boughtItems = this;
+    boughtItems.items = ShoppingService.boughtitems;
+    boughtItems.tobgfun = function () {
+      boughtItems.tobg = ShoppingService.tobgvalue;
+      return boughtItems.tobg;
+    };
+      //console.log(ShoppingService.tobgvalue);
+      // if(ShoppingService.boughtitems.length != 0){
+      // boughtItems.tobg = false;
+      // }
+  }
+
+  function ShoppingListCheckOffService(){
+    var service = this;
+    service.toBuyitems = [
     {
-        $scope.words = $scope.name.split(",");
-        $scope.words=$scope.words.filter(function(ele){return ele!=''});
-      if($scope.words.length==0)
-      {
-        $scope.message="Please enter data first";
-        $scope.customStyle.color= "red";
-          $scope.custom1Style.border= "1px solid red";
-      }
-      else if($scope.words.length<=3){
-        //console.log("hello");
-        $scope.message="Enjoy!";
-        $scope.customStyle.color= "green";
-        $scope.custom1Style.border= "1px solid green";
-      }
-      else{
-        $scope.message="Too Much!";
-        $scope.customStyle.color = "red";
-        $scope.custom1Style.border= "1px solid red";
-      }
+      name: "Milk",
+      quantity: "2"
+    },
+    {
+      name: "Donuts",
+      quantity: "200"
+    },
+    {
+      name: "Cookies",
+      quantity: "300"
+    },
+    {
+      name: "Chocolate",
+      quantity: "5"
     }
+  ];
+
+  service.boughtitems = [];
+
+  service.boughtfun = function (itemindex){
+    service.boughtitems.push(service.toBuyitems.splice(itemindex,1)[0]);
+    //onsole.log(service.boughtitems);
+  };
+  service.tobgvalue = true;
 }
-
-
 })();
